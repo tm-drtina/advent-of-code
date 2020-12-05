c45903ceb8c224d83e6b4453066f037c4f0eb357
Tomas Drtina <tomas.drtina@blindspot.ai>
2020-12-05 10:30:24 +0100

    Implemented y2020 star5.

---
+++ src/y2020/star5/part2.rs
@@ -0,0 +1,41 @@
+use itertools::Itertools;
+
+struct Seat {
+    row: i32,
+    col: i32,
+}
+
+impl Seat {
+    fn id(&self) -> i32 {
+        self.row * 8 + self.col
+    }
+}
+
+fn parse_row(s: &str) -> i32 {
+    (0..7)
+        .filter(|i| s.chars().nth(*i).unwrap() == 'B')
+        .map(|i| 2_i32.pow((6 - i) as u32))
+        .sum()
+}
+fn parse_col(s: &str) -> i32 {
+    (0..3)
+        .filter(|i| s.chars().nth(*i + 7).unwrap() == 'R')
+        .map(|i| 2_i32.pow((2 - i) as u32))
+        .sum()
+}
+
+pub fn run(input: &str) -> i32 {
+    input
+        .lines()
+        .map(|line| Seat {
+            row: parse_row(line),
+            col: parse_col(line),
+        })
+        .map(|seat| seat.id())
+        .sorted()
+        .tuple_windows::<(_, _)>()
+        .filter(|(a, b)| a + 1 != *b)
+        .map(|(a, _)| a + 1)
+        .next()
+        .unwrap()
+}
