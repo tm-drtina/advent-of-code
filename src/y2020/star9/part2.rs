cef1dac6ade64e5513faeca4931bf962ea70b585
Tomas Drtina <tomas.drtina@blindspot.ai>
2020-12-09 22:23:35 +0100

    Implemented y2020 star9.

---
+++ src/y2020/star9/part2.rs
@@ -0,0 +1,37 @@
+use std::str::FromStr;
+
+pub fn run(input: &str, preamble_size: usize) -> i64 {
+    let nums: Vec<i64> = input
+        .lines()
+        .map(|line| i64::from_str(line).unwrap())
+        .collect();
+
+    let target = (0..nums.len() - preamble_size - 1)
+        .map(|i| (nums[i + preamble_size], &nums[i..i + preamble_size]))
+        .filter(|(target, preamble)| {
+            !(0..preamble.len())
+                .any(|i| (i + 1..preamble.len()).any(|j| preamble[i] + preamble[j] == *target))
+        })
+        .next()
+        .unwrap()
+        .0;
+
+    let mut start_index = 0;
+    let mut end_index = 0;
+    let mut sum = nums[0];
+    loop {
+        if sum == target && end_index - start_index > 0 {
+            break;
+        }
+        if sum > target {
+            sum -= nums[start_index];
+            start_index += 1;
+        } else {
+            end_index += 1;
+            sum += nums[end_index]
+        }
+    }
+
+    nums[start_index..end_index + 1].iter().min().unwrap()
+        + nums[start_index..end_index + 1].iter().max().unwrap()
+}
