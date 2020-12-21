f5392142d1925442be0d3d6cf666df7e78883790
Tomas Drtina <tomas.drtina@blindspot.ai>
2020-12-21 23:20:22 +0100

    Implemented y2020 star21.

---
+++ src/y2020/star21/part1.rs
@@ -0,0 +1,39 @@
+use std::collections::{HashMap, HashSet};
+
+pub fn run(input: &str) -> usize {
+    let mut allergens: HashMap<&str, HashSet<&str>> = HashMap::new();
+    let mut ingredient_occurrences: HashMap<&str, usize> = HashMap::new();
+
+    input.lines().for_each(|line| {
+        let (ingredients_str, allergens_str) = line.split_once(" (contains ").unwrap();
+        let ingredients: HashSet<&str> = ingredients_str.split(" ").collect();
+        ingredients.iter().for_each(|ingredient| {
+            *ingredient_occurrences.entry(ingredient).or_default() += 1;
+        });
+        allergens_str[0..allergens_str.len() - 1]
+            .split(", ")
+            .for_each(|allergen| {
+                let prev_ingredients = allergens.get(allergen).map(|x| x.clone());
+                match prev_ingredients {
+                    Some(prev_ingredients) => {
+                        allergens.insert(
+                            allergen,
+                            prev_ingredients
+                                .intersection(&ingredients)
+                                .copied()
+                                .collect(),
+                        );
+                    }
+                    None => {
+                        allergens.insert(allergen, ingredients.clone());
+                    }
+                };
+            });
+    });
+
+    ingredient_occurrences
+        .into_iter()
+        .filter(|(key, _)| allergens.values().all(|x| !x.contains(key)))
+        .map(|(_, value)| value)
+        .sum()
+}
