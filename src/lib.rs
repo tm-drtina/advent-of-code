5b5c90d76cd5974541d0301483b6393aa056803d
Tomas Drtina <tm.drtina@gmail.com>
2021-12-07 17:49:05 +0100

    Create macro for running tests.

--- src/lib.rs
+++ src/lib.rs
@@ -12,3 +12,22 @@ mod utils;
 pub mod y2019;
 pub mod y2020;
 pub mod y2021;
+
+#[macro_export]
+macro_rules! aoc_test_suite {
+    ($func:path, $name:ident, $expected:expr, $input:expr) => {
+        #[test]
+        fn $name() {
+            let expected = $expected;
+            let start = std::time::Instant::now();
+            let actual = $func($input);
+            let elapsed = start.elapsed();
+            eprintln!("Test {} ran in {:#?}", stringify!($name), elapsed);
+            assert_eq!(expected, actual);
+        }
+    };
+    ($func:path, ($name:ident, $expected:expr, $input:expr), $(($name_tail:ident, $expected_tail:expr, $input_tail:expr)), +) => {
+        aoc_test_suite!($func, $name, $expected, $input);
+        aoc_test_suite!($func, $($name_tail, $expected_tail, $input_tail), +);
+    };
+}
--- src/y2021/star07/mod.rs
+++ src/y2021/star07/mod.rs
@@ -1,31 +1,20 @@
 pub mod part1;
 pub mod part2;
 
+#[macro_use]
 #[cfg(test)]
 mod tests {
-    #[test]
-    fn part1_sanity() {
-        let expected = 37;
-        let actual = super::part1::run(include_str!("input.sanity.txt"));
-        assert_eq!(expected, actual);
-    }
-    #[test]
-    fn part1_main() {
-        let expected = 343605;
-        let actual = super::part1::run(include_str!("input.txt"));
-        assert_eq!(expected, actual);
-    }
+    use crate::aoc_test_suite;
 
-    #[test]
-    fn part2_sanity() {
-        let expected = 168;
-        let actual = super::part2::run(include_str!("input.sanity.txt"));
-        assert_eq!(expected, actual);
-    }
-    #[test]
-    fn part2_main() {
-        let expected = 96744904;
-        let actual = super::part2::run(include_str!("input.txt"));
-        assert_eq!(expected, actual);
-    }
+    aoc_test_suite!(
+        super::part1::run,
+        (part1_main, 343605, include_str!("input.txt")),
+        (part1_sanity, 37, include_str!("input.sanity.txt"))
+    );
+
+    aoc_test_suite!(
+        super::part2::run,
+        (part2_main, 96744904, include_str!("input.txt")),
+        (part2_sanity, 168, include_str!("input.sanity.txt"))
+    );
 }
