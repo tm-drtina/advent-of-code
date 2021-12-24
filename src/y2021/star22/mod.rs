a03241871cf35a30b7c60ebeb0eca6e6921b88d3
Tomas Drtina <tomas.drtina@blindspot.ai>
2021-12-24 16:16:10 +0100

    y2021 star22.

---
+++ src/y2021/star22/mod.rs
@@ -0,0 +1,21 @@
+pub mod part1;
+pub mod part2;
+
+#[macro_use]
+#[cfg(test)]
+mod tests {
+    use crate::aoc_test_suite;
+
+    aoc_test_suite!(
+        super::part1::run,
+        (part1_main, 545118, include_str!("input.txt")),
+        (part1_sanity1, 39, include_str!("input.sanity1.txt")),
+        (part1_sanity2, 590784, include_str!("input.sanity2.txt")),
+    );
+
+    aoc_test_suite!(
+        super::part2::run,
+        (part2_main, 1227298136842375, include_str!("input.txt")),
+        (part2_sanity3, 2758514936282235, include_str!("input.sanity3.txt")),
+    );
+}
---
+++ src/y2021/star22/part2.rs
@@ -0,0 +1,13 @@
+use super::part1::{Command, Area};
+
+pub fn run(input: &str) -> usize {
+    let commands: Vec<Command> = input
+        .lines()
+        .map(|line| line.parse::<Command>().unwrap())
+        .collect();
+    let mut area = Area::new();
+    for command in commands {
+        area.step(command);
+    }
+    area.area()
+}
