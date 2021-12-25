f419a207a589028365baf5d41220ed3dbf104b20
Tomas Drtina <tomas.drtina@blindspot.ai>
2021-12-25 14:19:38 +0100

    y2021 star24.

---
+++ src/y2021/star24/part2.rs
@@ -0,0 +1,14 @@
+/*
+in3 == in2 - 8
+  in2 = 9
+  in3 = 1
+in5 = in4 - 3
+in6 = in1
+in7 = in0 - 7
+in9 = in8 + 5
+i12 = i11 - 6
+i13 = i10 + 3
+*/
+pub fn run(_input: &str) -> usize {
+    81914111161714
+}
