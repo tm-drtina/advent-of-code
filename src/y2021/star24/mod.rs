e0560437e0d232d9b5e67e2755a40f8aa4801323
Tomas Drtina <tomas.drtina@blindspot.ai>
2021-12-25 14:19:38 +0100

    y2021 star24.

---
+++ src/y2021/star24/mod.rs
@@ -0,0 +1,12 @@
+pub mod part1;
+pub mod part2;
+
+#[macro_use]
+#[cfg(test)]
+mod tests {
+    use crate::aoc_test_suite;
+
+    aoc_test_suite!(super::part1::run, (part1_main, 99919692496939, ""),);
+
+    aoc_test_suite!(super::part2::run, (part2_main, 81914111161714, ""),);
+}
