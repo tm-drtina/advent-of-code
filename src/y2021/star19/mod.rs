a2bf4681216dbad395970cdfacee1bd374a0804e
Tomas Drtina <tomas.drtina@blindspot.ai>
2021-12-25 16:31:55 +0100

    y2021 star19 - finish

--- src/y2021/star19/mod.rs
+++ src/y2021/star19/mod.rs
@@ -8,13 +8,13 @@ mod tests {
 
     aoc_test_suite!(
         super::part1::run,
-        (part1_main, 0, include_str!("input.txt")),
+        (part1_main, 326, include_str!("input.txt")),
         (part1_sanity, 79, include_str!("input.sanity.txt")),
     );
 
     aoc_test_suite!(
         super::part2::run,
-        (part2_main, 0, include_str!("input.txt")),
-        (part2_sanity, 0, include_str!("input.sanity.txt")),
+        (part2_main, 10630, include_str!("input.txt")),
+        (part2_sanity, 3621, include_str!("input.sanity.txt")),
     );
 }
--- src/y2021/star19/part2.rs
+++ src/y2021/star19/part2.rs
@@ -1,3 +1,9 @@
-pub fn run(_input: &str) -> usize {
-    0
+use super::part1::{AlignedSensor, Sensor};
+
+pub fn run(input: &str) -> usize {
+    let mut input = input.split("\n\n");
+    let mut aligned: AlignedSensor = input.next().unwrap().parse().unwrap();
+    let sensors: Vec<Sensor> = input.map(|scanner| scanner.parse().unwrap()).collect();
+    aligned.align_and_merge(sensors);
+    aligned.largest_distance()
 }
