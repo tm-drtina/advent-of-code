72b53cda2cbae5b0e8470320f39516b2e8c68250
Tomas Drtina <tomas.drtina@blindspot.ai>
2021-12-25 16:31:55 +0100

    y2021 star19 - finish

--- src/y2021/star19/part1.rs
+++ src/y2021/star19/part1.rs
@@ -1,32 +1,90 @@
-type Rotation = ((usize, usize, usize), (isize, isize, isize));
+use std::collections::HashSet;
 
-// TODO: set rotations
+use itertools::Itertools;
+
+#[derive(Debug, Clone, Copy)]
+enum Coord {
+    X,
+    Y,
+    Z,
+}
+type Rotation = ((Coord, Coord, Coord), (isize, isize, isize));
 const ROTATIONS: [Rotation; 24] = [
-    ((0, 1, 2), (1, 1, 1)); 24
+    ((Coord::X, Coord::Y, Coord::Z), (1, 1, 1)),
+    ((Coord::X, Coord::Z, Coord::Y), (1, 1, -1)),
+    ((Coord::X, Coord::Y, Coord::Z), (1, -1, -1)),
+    ((Coord::X, Coord::Z, Coord::Y), (1, -1, 1)),
+    ((Coord::X, Coord::Y, Coord::Z), (-1, -1, 1)),
+    ((Coord::X, Coord::Z, Coord::Y), (-1, -1, -1)),
+    ((Coord::X, Coord::Y, Coord::Z), (-1, 1, -1)),
+    ((Coord::X, Coord::Z, Coord::Y), (-1, 1, 1)),
+    ((Coord::Y, Coord::X, Coord::Z), (1, -1, 1)),
+    ((Coord::Y, Coord::Z, Coord::X), (1, -1, -1)),
+    ((Coord::Y, Coord::X, Coord::Z), (1, 1, -1)),
+    ((Coord::Y, Coord::Z, Coord::X), (1, 1, 1)),
+    ((Coord::Y, Coord::X, Coord::Z), (-1, 1, 1)),
+    ((Coord::Y, Coord::Z, Coord::X), (-1, 1, -1)),
+    ((Coord::Y, Coord::X, Coord::Z), (-1, -1, -1)),
+    ((Coord::Y, Coord::Z, Coord::X), (-1, -1, 1)),
+    ((Coord::Z, Coord::Y, Coord::X), (1, 1, -1)),
+    ((Coord::Z, Coord::X, Coord::Y), (1, 1, 1)),
+    ((Coord::Z, Coord::Y, Coord::X), (1, -1, 1)),
+    ((Coord::Z, Coord::X, Coord::Y), (1, -1, -1)),
+    ((Coord::Z, Coord::Y, Coord::X), (-1, 1, 1)),
+    ((Coord::Z, Coord::X, Coord::Y), (-1, 1, -1)),
+    ((Coord::Z, Coord::Y, Coord::X), (-1, -1, -1)),
+    ((Coord::Z, Coord::X, Coord::Y), (-1, -1, 1)),
 ];
 
 #[derive(Debug)]
 pub(super) struct Sensor {
     rotated_beacons: Vec<Vec<(isize, isize, isize)>>,
+    mismatch: HashSet<usize>,
 }
 
 #[derive(Debug)]
 pub(super) struct AlignedSensor {
-    beacons: Vec<(isize, isize, isize)>,
+    sub_sensors: Vec<Vec<(isize, isize, isize)>>,
+    distances: Vec<(isize, isize, isize)>,
 }
 
-fn rotate_beacons(_rotation: &Rotation, _beacons: &[(isize, isize, isize)]) -> Vec<(isize, isize, isize)> {
-    unimplemented!("Rotate beacon coordinates")
+fn coord(coord: Coord, orig: (isize, isize, isize)) -> isize {
+    match coord {
+        Coord::X => orig.0,
+        Coord::Y => orig.1,
+        Coord::Z => orig.2,
+    }
+}
+
+fn rotate_beacons(
+    rotation: &Rotation,
+    beacons: &[(isize, isize, isize)],
+) -> Vec<(isize, isize, isize)> {
+    beacons
+        .iter()
+        .copied()
+        .map(|orig| {
+            (
+                coord(rotation.0 .0, orig) * rotation.1 .0,
+                coord(rotation.0 .1, orig) * rotation.1 .1,
+                coord(rotation.0 .2, orig) * rotation.1 .2,
+            )
+        })
+        .collect()
 }
 
 impl std::str::FromStr for Sensor {
     type Err = ();
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let beacons = s.parse::<AlignedSensor>()?.beacons;
-        let rotated_beacons  = (&ROTATIONS).iter().map(|rotation| {
-            rotate_beacons(rotation, &beacons)
-        }).collect();
-        Ok(Self{ rotated_beacons })
+        let beacons = &s.parse::<AlignedSensor>()?.sub_sensors[0];
+        let rotated_beacons = (&ROTATIONS)
+            .iter()
+            .map(|rotation| rotate_beacons(rotation, beacons))
+            .collect();
+        Ok(Self {
+            rotated_beacons,
+            mismatch: HashSet::new(),
+        })
     }
 }
 
@@ -35,40 +93,120 @@ impl std::str::FromStr for AlignedSensor {
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         let mut lines = s.lines();
         lines.next(); // header
-        let beacons = lines.map(|line| {
-            let mut parts = line.split(",");
-            (
-                parts.next().unwrap().parse().unwrap(),
-                parts.next().unwrap().parse().unwrap(),
-                parts.next().unwrap().parse().unwrap(),
-            )
-        }).collect();
-        Ok(Self{ beacons })
+        let beacons = lines
+            .map(|line| {
+                let mut parts = line.split(",");
+                (
+                    parts.next().unwrap().parse().unwrap(),
+                    parts.next().unwrap().parse().unwrap(),
+                    parts.next().unwrap().parse().unwrap(),
+                )
+            })
+            .collect();
+        Ok(Self {
+            sub_sensors: vec![beacons],
+            distances: vec![(0, 0, 0)],
+        })
     }
 }
 
-impl AlignedSensor {
-    fn try_merge(&mut self, sensor: &Sensor) -> bool {
-        for _beacons in sensor.rotated_beacons.iter() {
-            unimplemented!("if beacons match, merge and return true")
+fn align(
+    orig: &[(isize, isize, isize)],
+    beacons: &[(isize, isize, isize)],
+) -> Option<(isize, isize, isize)> {
+    for p1 in orig {
+        for p2 in beacons {
+            let alignment = (p1.0 - p2.0, p1.1 - p2.1, p1.2 - p2.2);
+            let mut count = 0;
+            for beacon in beacons {
+                let aligned = (
+                    beacon.0 + alignment.0,
+                    beacon.1 + alignment.1,
+                    beacon.2 + alignment.2,
+                );
+                if orig.contains(&aligned) {
+                    count += 1;
+                }
+            }
+            if count >= 12 {
+                return Some(alignment);
+            }
         }
-        false
+    }
+    None
+}
+
+impl AlignedSensor {
+    fn try_align(
+        &mut self,
+        sensor: &mut Sensor,
+    ) -> Option<(Vec<(isize, isize, isize)>, (isize, isize, isize))> {
+        for (index, aligned_sub_sensor) in self.sub_sensors.iter().enumerate() {
+            if sensor.mismatch.contains(&index) {
+                continue;
+            }
+            for beacons in sensor.rotated_beacons.iter() {
+                if let Some(alignment) = align(aligned_sub_sensor, beacons) {
+                    return Some((
+                        beacons
+                            .iter()
+                            .copied()
+                            .map(|beacon| {
+                                (
+                                    beacon.0 + alignment.0,
+                                    beacon.1 + alignment.1,
+                                    beacon.2 + alignment.2,
+                                )
+                            })
+                            .collect(),
+                        alignment,
+                    ));
+                }
+            }
+            sensor.mismatch.insert(index);
+        }
+        None
+    }
+
+    pub(super) fn align_and_merge(&mut self, mut sensors: Vec<Sensor>) {
+        while !sensors.is_empty() {
+            for mut sensor in std::mem::replace(&mut sensors, Vec::new()) {
+                if let Some((aligned_sensor, alignment)) = self.try_align(&mut sensor) {
+                    self.sub_sensors.push(aligned_sensor);
+                    self.distances.push(alignment);
+                } else {
+                    sensors.push(sensor);
+                }
+            }
+        }
+    }
+
+    fn count_unique(&self) -> usize {
+        let unique = self
+            .sub_sensors
+            .iter()
+            .flatten()
+            .copied()
+            .collect::<HashSet<_>>();
+        unique.len()
+    }
+
+    pub(super) fn largest_distance(&self) -> usize {
+        self.distances
+            .iter()
+            .tuple_combinations()
+            .map(|(d1, d2)| {
+                ((d1.0 - d2.0).abs() + (d1.1 - d2.1).abs() + (d1.2 - d2.2).abs()) as usize
+            })
+            .max()
+            .unwrap()
     }
 }
 
 pub fn run(input: &str) -> usize {
     let mut input = input.split("\n\n");
     let mut aligned: AlignedSensor = input.next().unwrap().parse().unwrap();
-    let mut sensors: Vec<Sensor> = input
-        .map(|scanner| scanner.parse().unwrap())
-        .collect();
-
-    while !sensors.is_empty() {
-        for sensor in std::mem::replace(&mut sensors, Vec::new()) {
-            if !aligned.try_merge(&sensor) {
-                sensors.push(sensor);
-            }
-        }
-    }
-    aligned.beacons.len()
+    let sensors: Vec<Sensor> = input.map(|scanner| scanner.parse().unwrap()).collect();
+    aligned.align_and_merge(sensors);
+    aligned.count_unique()
 }
