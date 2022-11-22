a77e955ff3193b37aceca51845b2369b56dc821c
Tomas Drtina <tm.drtina@gmail.com>
2022-11-22 11:44:44 +0100

    Support multiple params to test function.

--- src/lib.rs
+++ src/lib.rs
@@ -12,32 +12,32 @@ pub mod y2021;
 #[cfg(not(feature = "ignore-sanity"))]
 #[macro_export]
 macro_rules! aoc_test_suite {
-    ($func:path, ($name:ident, $expected:expr, $input:expr) $(,)?) => {
+    ($func:path, ($name:ident, $expected:expr $(, $input:expr)+ $(,)?) $(,)?) => {
         #[test]
         fn $name() {
             let expected = $expected;
             let start = std::time::Instant::now();
-            let actual = $func($input);
+            let actual = $func($($input, )+);
             let elapsed = start.elapsed();
             eprintln!("Test {}::{} ran in {:#?}", module_path!(), stringify!($name), elapsed);
             assert_eq!(expected, actual);
         }
     };
-    ($func:path, ($name:ident, $expected:expr, $input:expr) $(, ($name_tail:ident, $expected_tail:expr, $input_tail:expr))+ $(,)?) => {
-        aoc_test_suite!($func, ($name, $expected, $input));
-        aoc_test_suite!($func $(, ($name_tail, $expected_tail, $input_tail))+);
+    ($func:path, ($name:ident, $expected:expr $(, $input:expr)+ $(,)?) $(, ($name_tail:ident, $expected_tail:expr $(, $input_tail:expr)+ $(,)?))+ $(,)?) => {
+        aoc_test_suite!($func, ($name, $expected $(, $input)+));
+        aoc_test_suite!($func $(, ($name_tail, $expected_tail $(, $input_tail)+))+);
     };
 }
 
 #[cfg(feature = "ignore-sanity")]
 #[macro_export]
 macro_rules! aoc_test_suite {
-    ($func:path, ($name:ident, $expected:expr, $input:expr) $(, ($name_tail:ident, $expected_tail:expr, $input_tail:expr))* $(,)?) => {
+    ($func:path, ($name:ident, $expected:expr $(, $input:expr)+ $(,)?) $(, ($name_tail:ident, $expected_tail:expr $(, $input_tail:expr)+ $(,)?))* $(,)?) => {
         #[test]
         fn $name() {
             let expected = $expected;
             let start = std::time::Instant::now();
-            let actual = $func($input);
+            let actual = $func($($input, )+);
             let elapsed = start.elapsed();
             eprintln!(
                 "Test {}::{} ran in {:#?}",
