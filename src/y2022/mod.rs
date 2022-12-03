c5281b3e3ce32f927a396c50d3fe6667351697f3
Tomáš Drtina <tm.drtina@gmail.com>
2022-12-03 11:48:06 +0100

    Implement `y2022s03` (#8)

--- src/y2022/mod.rs
+++ src/y2022/mod.rs
@@ -1,6 +1,6 @@
 pub mod star01;
 pub mod star02;
-// pub mod star03;
+pub mod star03;
 // pub mod star04;
 // pub mod star05;
 // pub mod star06;
---
+++ src/y2022/star03/part2.rs
@@ -0,0 +1,56 @@
+use std::str::Lines;
+
+use anyhow::{bail, Error, Result};
+use itertools::{Chunk, Itertools};
+
+struct Group {
+    elves: Vec<Vec<u8>>,
+}
+
+impl<'a> TryFrom<Chunk<'a, Lines<'a>>> for Group {
+    type Error = Error;
+
+    fn try_from(lines: Chunk<'a, Lines<'a>>) -> Result<Self, Self::Error> {
+        let elves = lines
+            .map(|l| {
+                l.bytes()
+                    .map(|b| {
+                        Ok(match b {
+                            b'a'..=b'z' => b - b'a' + 1,
+                            b'A'..=b'Z' => b - b'A' + 27,
+                            _ => bail!("Unexpected char"),
+                        })
+                    })
+                    .collect()
+            })
+            .collect::<Result<Vec<_>>>()?;
+        Ok(Self { elves })
+    }
+}
+
+impl Group {
+    fn priority(&self) -> usize {
+        for item in &self.elves[0] {
+            if self.elves[1].iter().any(|i| item == i) && self.elves[2].iter().any(|i| item == i) {
+                return *item as usize;
+            }
+        }
+        unreachable!("Every group should have duplicate")
+    }
+}
+
+fn run_safe(input: &str) -> Result<usize> {
+    Ok(input
+        .lines()
+        .chunks(3)
+        .into_iter()
+        .map(TryFrom::try_from)
+        .collect::<Result<Vec<_>>>()?
+        .iter()
+        .map(Group::priority)
+        .sum())
+}
+
+pub fn run(input: &str) -> usize {
+    run_safe(input).unwrap()
+}
