2891d07b008066faaa3b9348dfc58b6fc8e12581
Tomas Drtina <tm.drtina@gmail.com>
2022-12-04 11:31:11 +0100

    Implement `y2022s04`

---
+++ src/y2022/star04/part1.rs
@@ -0,0 +1,13 @@
+use anyhow::Result;
+
+use super::common::Pair;
+
+pub fn run(input: &str) -> Result<usize> {
+    Ok(input
+        .lines()
+        .map(str::parse::<Pair>)
+        .collect::<Result<Vec<_>>>()?
+        .into_iter()
+        .filter(Pair::self_contained)
+        .count())
+}
