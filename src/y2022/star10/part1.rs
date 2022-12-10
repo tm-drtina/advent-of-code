5ae84371ce5cfe93edcc4dfc3da6afb955aacdfc
Tomáš Drtina <tm.drtina@gmail.com>
2022-12-10 14:01:51 +0100

    Implement `y2022s10` (#16)

---
+++ src/y2022/star10/part1.rs
@@ -0,0 +1,56 @@
+use anyhow::{bail, Result};
+
+struct State {
+    x: i32,
+    counter: i32,
+    result: i32,
+    next_result_counter: i32,
+}
+
+impl State {
+    fn new() -> Self {
+        Self {
+            x: 1,
+            counter: 0,
+            result: 0,
+            next_result_counter: 20,
+        }
+    }
+
+    fn compute_result(&mut self) {
+        if self.counter >= self.next_result_counter {
+            self.result += self.x * (self.counter - (self.counter % 20));
+            self.next_result_counter += 40;
+        }
+    }
+
+    fn noop(&mut self) {
+        self.counter += 1;
+        self.compute_result();
+    }
+
+    fn addx(&mut self, amount: i32) {
+        self.counter += 2;
+        self.compute_result();
+        self.x += amount;
+    }
+
+    fn step(&mut self, line: &str) -> Result<()> {
+        match line {
+            "noop" => self.noop(),
+            _ if line.starts_with("addx ") => {
+                self.addx(line.strip_prefix("addx ").unwrap().parse()?);
+            }
+            _ => bail!("Unknown command {}", line),
+        }
+        Ok(())
+    }
+}
+
+pub fn run(input: &str) -> Result<i32> {
+    let mut state = State::new();
+    for line in input.lines() {
+        state.step(line)?;
+    }
+    Ok(state.result)
+}
---
+++ src/y2022/star10/part2.rs
@@ -0,0 +1,58 @@
+use anyhow::{bail, Result};
+
+struct State {
+    x: i32,
+    counter: i32,
+    result: String,
+}
+
+impl State {
+    fn new() -> Self {
+        Self {
+            x: 1,
+            counter: 0,
+            result: String::new(),
+        }
+    }
+
+    fn write_res(&mut self) {
+        if self.x.abs_diff(self.counter % 40) < 2 {
+            self.result.push('#');
+        } else {
+            self.result.push('.');
+        }
+        self.counter += 1;
+        if self.counter % 40 == 0 {
+            self.result.push('\n');
+        }
+    }
+
+    fn noop(&mut self) {
+        self.write_res();
+    }
+
+    fn addx(&mut self, amount: i32) {
+        self.write_res();
+        self.write_res();
+        self.x += amount;
+    }
+
+    fn step(&mut self, line: &str) -> Result<()> {
+        match line {
+            "noop" => self.noop(),
+            _ if line.starts_with("addx ") => {
+                self.addx(line.strip_prefix("addx ").unwrap().parse()?);
+            }
+            _ => bail!("Unknown command {}", line),
+        }
+        Ok(())
+    }
+}
+
+pub fn run(input: &str) -> Result<String> {
+    let mut state = State::new();
+    for line in input.lines() {
+        state.step(line)?;
+    }
+    Ok(state.result)
+}
