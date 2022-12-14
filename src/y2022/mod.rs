785baea5cd434efde3dfdefb717b8081acc9c9b8
Tomas Drtina <tomas.drtina@blindspot.ai>
2022-12-14 10:17:31 +0100

    Implement `y2022s14`

--- src/y2022/mod.rs
+++ src/y2022/mod.rs
@@ -11,7 +11,7 @@ pub mod star10;
 pub mod star11;
 pub mod star12;
 pub mod star13;
-// pub mod star14;
+pub mod star14;
 // pub mod star15;
 // pub mod star16;
 // pub mod star17;
---
+++ src/y2022/star14/part1.rs
@@ -0,0 +1,89 @@
+use std::collections::HashSet;
+use std::str::FromStr;
+
+use anyhow::{anyhow, Error, Result};
+use itertools::Itertools;
+
+use crate::utils::point::Point2D;
+
+type Point = Point2D<u32>;
+
+struct Puzzle {
+    blocked: HashSet<Point>,
+    max_y: u32,
+}
+
+impl FromStr for Puzzle {
+    type Err = Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut blocked = HashSet::new();
+        let mut max_y = 0;
+        for line in s.lines() {
+            for (from, to) in line.split(" -> ").tuple_windows() {
+                let (from_x, from_y) = from.split_once(',').ok_or(anyhow!("Invalid format"))?;
+                let mut from_x = from_x.parse()?;
+                let mut from_y = from_y.parse()?;
+                let (to_x, to_y) = to.split_once(',').ok_or(anyhow!("Invalid format"))?;
+                let mut to_x = to_x.parse()?;
+                let mut to_y = to_y.parse()?;
+
+                if from_y > to_y {
+                    std::mem::swap(&mut from_y, &mut to_y);
+                }
+                if from_x > to_x {
+                    std::mem::swap(&mut from_x, &mut to_x);
+                }
+
+                if to_y > max_y {
+                    max_y = to_y;
+                }
+
+                for x in from_x..=to_x {
+                    for y in from_y..=to_y {
+                        blocked.insert(Point2D { x, y });
+                    }
+                }
+            }
+        }
+        Ok(Self { blocked, max_y })
+    }
+}
+
+impl Puzzle {
+    fn simulate(&self) -> Option<Point> {
+        let mut pos = Point2D { x: 500, y: 0 };
+        loop {
+            if pos.y >= self.max_y {
+                return None;
+            }
+            if !self.blocked.contains(&pos.bottom()) {
+                pos = pos.bottom();
+            } else if !self.blocked.contains(&pos.bottom_left()) {
+                pos = pos.bottom_left();
+            } else if !self.blocked.contains(&pos.bottom_right()) {
+                pos = pos.bottom_right();
+            } else {
+                return Some(pos);
+            }
+        }
+    }
+}
+
+impl Iterator for Puzzle {
+    type Item = ();
+
+    fn next(&mut self) -> Option<Self::Item> {
+        match self.simulate() {
+            Some(pt) => {
+                self.blocked.insert(pt);
+                Some(())
+            }
+            None => None,
+        }
+    }
+}
+
+pub fn run(input: &str) -> Result<usize> {
+    Ok(input.parse::<Puzzle>()?.count())
+}
