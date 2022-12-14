a8f495a44aa780ed38fb186a72a7334dae4f9413
Tomas Drtina <tm.drtina@gmail.com>
2022-12-14 02:26:01 +0100

    Implement `y2022s13`

---
+++ src/y2022/star13/mod.rs
@@ -0,0 +1,20 @@
+mod common;
+pub mod part1;
+pub mod part2;
+
+#[cfg(test)]
+mod tests {
+    use crate::aoc_test_suite;
+
+    aoc_test_suite!(
+        super::part1::run,
+        (part1_main, 5366, include_str!("input.txt")),
+        (part1_sanity1, 13, include_str!("input.sanity1.txt")),
+    );
+
+    aoc_test_suite!(
+        super::part2::run,
+        (part2_main, 23391, include_str!("input.txt")),
+        (part2_sanity1, 140, include_str!("input.sanity1.txt")),
+    );
+}
