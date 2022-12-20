37ac04d6b431968671ac773f66470fdbef723bc1
Tomas Drtina <tm.drtina@gmail.com>
2022-12-21 00:53:14 +0100

    fmt

--- src/y2022/star20/common.rs
+++ src/y2022/star20/common.rs
@@ -3,7 +3,7 @@ use std::str::FromStr;
 use anyhow::{anyhow, Error, Result};
 
 #[derive(Debug, Clone, Copy)]
-struct Value{
+struct Value {
     value: i64,
     position: usize,
 }
@@ -16,12 +16,15 @@ impl FromStr for Puzzle {
 
     fn from_str(s: &str) -> Result<Self> {
         Ok(Self(
-            s.lines().enumerate().map(|(index, s)| {
-                Ok(Value {
-                    value: s.parse()?,
-                    position: index,
+            s.lines()
+                .enumerate()
+                .map(|(index, s)| {
+                    Ok(Value {
+                        value: s.parse()?,
+                        position: index,
+                    })
                 })
-            }).collect::<Result<_>>()?
+                .collect::<Result<_>>()?,
         ))
     }
 }
@@ -29,35 +32,47 @@ impl FromStr for Puzzle {
 impl Puzzle {
     pub(super) fn mix(&mut self) {
         for i in 0..self.0.len() {
-            let Value{value, position} = self.0[i];
+            let Value { value, position } = self.0[i];
 
             let v = value % (self.0.len() - 1) as i64;
 
+            #[allow(clippy::comparison_chain)]
             if v == 0 {
-                continue;
+                // nothing to do
             } else if v > 0 {
                 let v = v as usize;
                 if v + position >= self.0.len() {
                     // overflow case
-                    let foo = self.0.len() - position; 
-                    let new_pos = v + 1 - foo;
-                    self.0.iter_mut().filter(|v| v.position < position && v.position >= new_pos).for_each(|v| v.position += 1);
+                    let new_pos = v + 1 + position - self.0.len();
+                    self.0
+                        .iter_mut()
+                        .filter(|v| v.position < position && v.position >= new_pos)
+                        .for_each(|v| v.position += 1);
                     self.0[i].position = new_pos;
                 } else {
                     let new_pos = position + v;
-                    self.0.iter_mut().filter(|v| v.position > position && v.position <= new_pos).for_each(|v| v.position -= 1);
+                    self.0
+                        .iter_mut()
+                        .filter(|v| v.position > position && v.position <= new_pos)
+                        .for_each(|v| v.position -= 1);
                     self.0[i].position = new_pos;
                 }
             } else {
                 let v = (-v) as usize;
                 if position >= v {
                     let new_pos = position - v;
-                    self.0.iter_mut().filter(|v| v.position < position && v.position >= new_pos).for_each(|v| v.position += 1);
+                    self.0
+                        .iter_mut()
+                        .filter(|v| v.position < position && v.position >= new_pos)
+                        .for_each(|v| v.position += 1);
                     self.0[i].position = new_pos;
                 } else {
                     // overflow
                     let new_pos = self.0.len() + position - v - 1;
-                    self.0.iter_mut().filter(|v| v.position > position && v.position <= new_pos).for_each(|v| v.position -= 1);
+                    self.0
+                        .iter_mut()
+                        .filter(|v| v.position > position && v.position <= new_pos)
+                        .for_each(|v| v.position -= 1);
                     self.0[i].position = new_pos;
                 }
             }
@@ -65,13 +80,18 @@ impl Puzzle {
     }
 
     pub(super) fn apply_decryption_key(&mut self, key: i64) {
-        self.0.iter_mut().for_each(|v| v.value = v.value * key);
+        self.0.iter_mut().for_each(|v| v.value *= key);
     }
 
     pub(super) fn groove(self) -> Result<i64> {
         let mut values = self.0;
         values.sort_unstable_by_key(|v| v.position);
-        let start = values.iter().position(|v| v.value == 0).ok_or(anyhow!("Zero value not found"))?;
-        Ok(values[(start + 1000) % values.len()].value + values[(start + 2000) % values.len()].value + values[(start + 3000) % values.len()].value)
+        let start = values
+            .iter()
+            .position(|v| v.value == 0)
+            .ok_or(anyhow!("Zero value not found"))?;
+        Ok(values[(start + 1000) % values.len()].value
+            + values[(start + 2000) % values.len()].value
+            + values[(start + 3000) % values.len()].value)
     }
 }
--- src/y2022/star20/part2.rs
+++ src/y2022/star20/part2.rs
@@ -4,7 +4,7 @@ use super::common::Puzzle;
 
 pub fn run(input: &str) -> Result<i64> {
     let mut puzzle: Puzzle = input.parse()?;
-    puzzle.apply_decryption_key(811589153);
+    puzzle.apply_decryption_key(811_589_153);
     for _ in 0..10 {
         puzzle.mix();
     }
