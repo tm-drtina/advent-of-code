65f628c4608fbda5e45a4aa27acb05af04ecd833
Tomáš Drtina <tm.drtina@gmail.com>
2022-12-24 15:37:31 +0100

    Implement `y2022s24` (#29)

--- src/y2022/mod.rs
+++ src/y2022/mod.rs
@@ -21,5 +21,5 @@ pub mod star20;
 pub mod star21;
 // pub mod star22;
 pub mod star23;
-// pub mod star24;
+pub mod star24;
 // pub mod star25;
---
+++ src/y2022/star24/part1.rs
@@ -0,0 +1,16 @@
+use anyhow::{anyhow, Result};
+
+use super::common::{Map, State};
+
+pub fn run(input: &str) -> Result<usize> {
+    let mut map: Map = input.parse()?;
+    let mut state = State::start();
+    for i in 1.. {
+        map = map.step();
+        state = state.step(&map);
+        if state.is_final(&map) {
+            return Ok(i);
+        }
+    }
+    Err(anyhow!("Solution not found"))
+}
