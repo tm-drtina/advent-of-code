f492fe81012e7c7c30adba97e374953efbd64808
Tomas Drtina <tomas.drtina@blindspot.ai>
2023-12-01 06:39:12 +0100

    y2023::star01::part1

---
+++ src/y2023/star01/part1.rs
@@ -0,0 +1,18 @@
+use anyhow::{anyhow, Result};
+
+fn parse_line(line: &str) -> Result<(u32, u32)> {
+    let mut iter = line.chars().filter_map(|c| c.to_digit(10));
+    let first = iter.next().ok_or(anyhow!(""))?;
+    let last = iter.last().unwrap_or(first);
+    Ok((first, last))
+}
+
+pub fn run(input: &str) -> Result<u32> {
+    Ok(input
+        .lines()
+        .map(parse_line)
+        .collect::<Result<Vec<_>>>()?
+        .into_iter()
+        .map(|(a, b)| a * 10 + b)
+        .sum())
+}
