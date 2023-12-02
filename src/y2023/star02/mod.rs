540ba5664985dd848b5011200a3d140875230d60
Tomáš Drtina <tm.drtina@gmail.com>
2023-12-02 12:15:15 +0100

    y2023::star02 (#37)

---
+++ src/y2023/star02/mod.rs
@@ -0,0 +1,19 @@
+pub mod part1;
+pub mod part2;
+
+#[cfg(test)]
+mod tests {
+    use crate::aoc_test_suite;
+
+    aoc_test_suite!(
+        super::part1::run,
+        (part1_main, 2256, include_str!("input.txt")),
+        (part1_sanity1, 8, include_str!("input.sanity1.txt")),
+    );
+
+    aoc_test_suite!(
+        super::part2::run,
+        (part2_main, 74229, include_str!("input.txt")),
+        (part2_sanity1, 2286, include_str!("input.sanity1.txt")),
+    );
+}
