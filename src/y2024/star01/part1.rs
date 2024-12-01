09668474feae191cd1d6fbb2ae50ef9a8c61bead
Tomas Drtina <tm.drtina@gmail.com>
2024-12-01 11:18:00 +0100

    y2024::star01::part1

---
+++ src/y2024/star01/part1.rs
@@ -0,0 +1,27 @@
+use anyhow::{Context, Result};
+
+pub fn run(input: &str) -> Result<usize> {
+    let mut left = Vec::<usize>::new();
+    let mut right = Vec::<usize>::new();
+    for line in input.lines() {
+        let mut split = line.split_ascii_whitespace();
+        left.push(
+            split
+                .next()
+                .context("No data on the line")?
+                .parse()
+                .context("Failed to parse input number")?,
+        );
+        right.push(
+            split
+                .next()
+                .context("No sencond number on the line")?
+                .parse()
+                .context("Failed to parse input number")?,
+        );
+    }
+    left.sort();
+    right.sort();
+
+    Ok(left.into_iter().zip(right).map(|(l, r)| l.abs_diff(r)).sum())
+}
