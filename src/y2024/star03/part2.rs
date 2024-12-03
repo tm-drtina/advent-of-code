5c9181756956fccac8528cc95c25da2e3757f382
Tomas Drtina <tdrtina-ext@arista.com>
2024-12-03 10:53:05 +0100

    fmt

--- src/y2024/star03/part2.rs
+++ src/y2024/star03/part2.rs
@@ -2,7 +2,9 @@ use anyhow::Result;
 use regex::Regex;
 
 enum Op {
-    Do, Dont, Mul(usize, usize)
+    Do,
+    Dont,
+    Mul(usize, usize),
 }
 
 pub fn run(input: &str) -> Result<usize> {
@@ -11,24 +13,19 @@ pub fn run(input: &str) -> Result<usize> {
     Ok(re
         .captures_iter(input)
         .map(|c| c.extract())
-        .map(|(_, [op, a, b])| {
-            match op {
-                "mul" => Ok(Op::Mul(a.parse()?, b.parse()?)),
-                "do()" => Ok(Op::Do),
-                "don't()" => Ok(Op::Dont),
-                _ => unreachable!()
-            }
+        .map(|(_, [op, a, b])| match op {
+            "mul" => Ok(Op::Mul(a.parse()?, b.parse()?)),
+            "do()" => Ok(Op::Do),
+            "don't()" => Ok(Op::Dont),
+            _ => unreachable!(),
         })
         .collect::<Result<Vec<Op>>>()?
         .into_iter()
-        .fold((true, 0), |(enabled, sum), op| {
-            match op {
-                Op::Do => (true, sum),
-                Op::Dont => (false, sum),
-                Op::Mul(a, b) if enabled => (enabled, sum + a * b),
-                Op::Mul(_, _) => (enabled, sum),
-            }
+        .fold((true, 0), |(enabled, sum), op| match op {
+            Op::Do => (true, sum),
+            Op::Dont => (false, sum),
+            Op::Mul(a, b) if enabled => (enabled, sum + a * b),
+            Op::Mul(_, _) => (enabled, sum),
         })
-        .1
-    )
+        .1)
 }
