bbc946564ae4f7dedcdcd7fbdb850bbec0a3c0c7
Tomas Drtina <tm.drtina@gmail.com>
2024-12-13 14:22:28 +0100

    y2024::star09::part1

--- src/y2024/mod.rs
+++ src/y2024/mod.rs
@@ -6,7 +6,7 @@ pub mod star05;
 pub mod star06;
 pub mod star07;
 pub mod star08;
-// pub mod star09;
+pub mod star09;
 // pub mod star10;
 // pub mod star11;
 // pub mod star12;
---
+++ src/y2024/star09/part1.rs
@@ -0,0 +1,89 @@
+use std::collections::VecDeque;
+
+use anyhow::Result;
+
+#[derive(Debug, Clone, Copy)]
+enum Field {
+    Data { value: usize, length: usize },
+    Empty(usize),
+}
+impl From<(usize, char)> for Field {
+    fn from((index, length): (usize, char)) -> Self {
+        let length = (length as u8 - b'0') as usize;
+        if index & 1 > 0 {
+            Self::Empty(length)
+        } else {
+            Self::Data {
+                value: index / 2,
+                length,
+            }
+        }
+    }
+}
+impl Field {
+    fn length(&self) -> usize {
+        match self {
+            Field::Data { length, .. } => *length,
+            Field::Empty(length) => *length,
+        }
+    }
+}
+
+struct CompressionIterator {
+    data: VecDeque<Field>
+}
+
+impl CompressionIterator {
+    pub fn new(data: VecDeque<Field>) -> Self {
+        Self { 
+            data,
+        }
+    }
+}
+
+impl Iterator for CompressionIterator {
+    type Item = usize;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        while self.data.front()?.length() == 0 {
+            self.data.pop_front();
+        }
+
+        let first = self.data.front_mut()?;
+        match first {
+            Field::Data { value, length } => {
+                *length -= 1;
+                Some(*value)
+            },
+            Field::Empty(length) => {
+                *length -= 1;
+                while matches!(self.data.back()?, Field::Empty(_) | Field::Data {length: 0, ..}) {
+                    self.data.pop_back();
+                }
+
+                let last = self.data.back_mut()?;
+                match last {
+                    Field::Data { value, length } => {
+                        *length -= 1;
+                        Some(*value)
+                    },
+                    Field::Empty(_) => unreachable!("Filtered in previous loop"),
+                }
+
+            },
+        }
+    }
+}
+
+pub fn run(input: &'static str) -> Result<usize> {
+    let data = input
+        .chars()
+        .enumerate()
+        .map(Field::from)
+        .collect::<VecDeque<_>>();
+
+    Ok(CompressionIterator::new(data)
+        .enumerate()
+        .map(|(i, value)| i * value)
+        .sum())
+}
